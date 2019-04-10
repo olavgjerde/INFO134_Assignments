de176b8c62c94042a67240c8805f498d6c4276f1
Fruithut <fruithut@tutanota.com>
2019-04-10 16:56:46 +0200

    Added navigation and section toggling

--- Solution/js/folk.js
+++ Solution/js/folk.js
@@ -1,16 +1,48 @@
-let informationSection = document.getElementById("information");
+let introSection = document.getElementById("introduction");
 let overviewSection = document.getElementById("overview");
 let detailsSection = document.getElementById("details");
 let comparisonSection = document.getElementById("comparison");
+let sectionList = [introSection, overviewSection, detailsSection, comparisonSection];
 
-let infoNavButton = document.getElementById("infoButton");
+let introNavButton = document.getElementById("introButton");
 let detailsNavButton = document.getElementById("detailsButton");
 let overviewNavButton = document.getElementById("overviewButton");
 let comparisonNavButton = document.getElementById("comparisonButton");
 
-let sectionList = [informationSection, overviewSection, detailsSection, comparisonSection];
+/**
+ * Sets the initial condition for the application
+ *  - Hiding all sections except the introduction
+ *  - Adding eventlisteners where needed
+ */
+function init() {
+    introNavButton.addEventListener("click", function() {
+	    toggleSectionVisibility(introSection);
+    });
+    detailsNavButton.addEventListener("click", function() {
+	    toggleSectionVisibility(detailsSection);
+    });
+    overviewNavButton.addEventListener("click", function() {
+	    toggleSectionVisibility(overviewSection);
+    });
+    comparisonNavButton.addEventListener("click", function() {
+	    toggleSectionVisibility(comparisonSection);
+    });
 
-function toggleSection(targetSection, currentSection) {
-  targetSection.classList.toggle("hideSection");
-  currentSection.classList.toggle("hideSection");
+    overviewSection.classList.toggle("hiddenSection");
+    detailsSection.classList.toggle("hiddenSection");
+    comparisonSection.classList.toggle("hiddenSection");
+}
+
+/**
+ * Shows a given section and hides all the other ones defined in "sectionList"
+ * @param targetSection section-element which should become visible
+ */
+function toggleSectionVisibility(targetSection) {
+    targetSection.classList.remove("hiddenSection");
+    for (var sectionId in sectionList) {
+	    let sectionInList = sectionList[sectionId];
+        if (sectionInList !== targetSection) {
+	        sectionInList.classList.add("hiddenSection");
+        }
+    }
 }
